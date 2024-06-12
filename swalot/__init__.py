a75148bcb589ff775044ddb3915067e6632bc561
cpfy <309301477@qq.com>
2024-06-12 16:19:44 +0800

    feat: time protect with testcase

--- swalot/__init__.py
+++ swalot/__init__.py
@@ -1,5 +1,9 @@
 from .swalot import MemoryProtector, AutoMemoryProtector
+from .time import TimeController
 
-def protect(remain=256, device=0):
+def gpu(remain=256, device=0):
     protector = MemoryProtector(remain, device)
     return AutoMemoryProtector(protector)
+
+def time(speed=1.5, offset=0, random=False, seed=None):
+    return TimeController(speed, random, seed)
